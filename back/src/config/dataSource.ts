ada229573162be4de330b29a44e74f407f16bd11
Ignacia22 <mfernandezpolanco@gmail.com>
2025-06-30 16:17:55 -0400

    settings

--- back/src/config/dataSource.ts
+++ back/src/config/dataSource.ts
@@ -1,22 +1,25 @@
 import { DataSource } from "typeorm";
-import { DB_HOST, DB_NAME, DB_PASSWORD, DB_PORT, DB_USER } from "./envs";
+
 import { User } from "../entities/User";
 import { Credential } from "../entities/Credential";
 import { Order } from "../entities/Order";
 import { Category } from "../entities/Category";
 import { Product } from "../entities/Product";
+import { DATABASE_URL } from "./envs";
 
 export const AppDataSource = new DataSource({
   type: "postgres",
-  host: DB_HOST,
-  port: DB_PORT,
-  username: DB_USER,
-  password: DB_PASSWORD,
-  database: DB_NAME,
+  url: DATABASE_URL, // Usar la URL completa en lugar de campos separados
   synchronize: true,
   //dropSchema: true,
   logging: false,
   entities: [User, Credential, Order, Product, Category],
   subscribers: [],
   migrations: [],
+  ssl: true,
+  extra: {
+    ssl: {
+      rejectUnauthorized: false
+    }
+  }
 });
